c534029a36f6d3178b67466d5b4f2f931c39b3b7
waibaBikash <kavrelibikash@gmail.com>
2025-07-07 23:19:59 +1000

    feat: enhance Navbar component with search input and navigation buttons

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from 'react'
 import { assets, menuLinks } from '../assets/assets'
-import { Link, useLocation } from 'react-router-dom'
+import { Link, useLocation, useNavigate } from 'react-router-dom'
 
-const Navbar = () => {
+const Navbar = ({setShowLogin}) => {
 
   const location = useLocation();
   const [open, setOpen] = useState(false);
+  const navigate = useNavigate();
   return (
     <div className={`flex items-center justify-between px-6 md:px-16 lg:px-24 xl:px-32 py-4 text-gray-600 border-b border-borderColor relative transition-all ${location.pathname === '/' && 'bg-light'}`}>
       <Link to='/'>
@@ -19,7 +20,21 @@ const Navbar = () => {
             {link.name}
           </Link>
          ))}
+
+         <div className='hidden lg:flex items-center text-sm gap-2 border border-borderColor px-3 rounded-full max-w-56'>
+            <input type="text" className='py-1.5 w-full bg-transparent outline-none placeholder-gray-500 ' placeholder='Search products' />
+            <img src={assets.search_icon} alt="" />
+         </div>
+
+           <div className='flex max-sm:flex-col items-start sm:items-center gap-6'>
+             <button onClick={()=> navigate('/owner')} className='cursor-pointer'>Dashboard</button>
+             <button onClick={()=> setShowLogin(true) } className='cursor-pointer px-8 py-2 bg-primary hover:bg-primary-dull transition-all text-white rounded-lg'>Login</button>
+           </div>
       </div>
+        
+        <button className='sm:hidden cursor-pointer' aria-level='Menu' onClick={()=> setOpen(!open)}>
+           <img src={open ? assets.close_icon : assets.menu_icon} alt="menu" />
+        </button>
     </div>
   )
 }
