373a5380361550e0fec6c80055d9644152bce6a6
waibaBikash <kavrelibikash@gmail.com>
2025-07-07 23:24:39 +1000

    feat: update App component to conditionally render Navbar with login state management

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './components/Navbar'
+import { useLocation } from 'react-router-dom';
 
 const App = () => {
+
+  const [showLogin, setShowLogin] = useState(false);
+  const isOwnerPath = useLocation().pathname.startsWith('/owner');
   return (
-    <div>
-      <Navbar />
-    </div>
+    <>
+      {!isOwnerPath && <Navbar setShowLogin={setShowLogin} />}
+    </>
   )
 }
 
