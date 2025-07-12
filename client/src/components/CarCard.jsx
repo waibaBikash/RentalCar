042645ae939c4361e87a7372b0703e79b1deaa4c
waibaBikash <kavrelibikash@gmail.com>
2025-07-12 21:18:26 +1000

    feat: enable navigation to car details on click in CarCard component

--- client/src/components/CarCard.jsx
+++ client/src/components/CarCard.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { assets } from '../assets/assets'
+import { useNavigate } from 'react-router-dom';
 
 const CarCard = ({car}) => {
 
   const currency = import.meta.env.VITE_CURRENCY;
+  const navigate = useNavigate();
   return (
-    <div className='group rounded-xl overflow-hidden shadow-lg hover:-translate-y-1 transition-all duration-500 cursor-pointer'>
+    <div onClick={()=> {navigate(`/cars-details/${car._id}`); scrollTo(0,0)}} className='group rounded-xl overflow-hidden shadow-lg hover:-translate-y-1 transition-all duration-500 cursor-pointer'>
 
       <div className='relative h-48 overflow-hidden'>
           <img src={car.image} alt="Car Image" className='w-full h-full object-cover transition-transform duration-500 group-hover:scale-105' />
