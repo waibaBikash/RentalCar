749525aefbe621e31eea4c8c5e6362d380852c5a
waibaBikash <kavrelibikash@gmail.com>
2025-07-12 22:26:38 +1000

    style: improve formatting and readability in CarCard component

--- client/src/components/CarCard.jsx
+++ client/src/components/CarCard.jsx
@@ -7,12 +7,15 @@ const CarCard = ({car}) => {
   const currency = import.meta.env.VITE_CURRENCY;
   const navigate = useNavigate();
   return (
-    <div onClick={()=> {navigate(`/cars-details/${car._id}`); scrollTo(0,0)}} className='group rounded-xl overflow-hidden shadow-lg hover:-translate-y-1 transition-all duration-500 cursor-pointer'>
+    <div onClick={()=> {navigate(`/cars-details/${car._id}`); scrollTo(0,0)}}
+     className='group rounded-xl overflow-hidden shadow-lg hover:-translate-y-1 transition-all duration-500 cursor-pointer'>
 
       <div className='relative h-48 overflow-hidden'>
           <img src={car.image} alt="Car Image" className='w-full h-full object-cover transition-transform duration-500 group-hover:scale-105' />
 
-          {car.isAvailable && <p className='absolute top-4 left-4 bg-primary/90 text-white text-xs px-2.5 py-1 rounded-full'>Available Now</p>}
+          {car.isAvailable && <p 
+          className='absolute top-4 left-4 bg-primary/90 text-white text-xs px-2.5 py-1 rounded-full'>
+          Available Now</p>}
 
           <div className='absolute bottom-4 right-4 bg-black/80 backdrop-blur-sm text-white px-3 py-2 rounded-lg'>
               <span className='font-semibold'>{currency}{car.pricePerDay}</span>
