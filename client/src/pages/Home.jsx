dcecb8aed2ba951ba88cbadb343a4e5bc18d45eb
waibaBikash <kavrelibikash@gmail.com>
2025-07-12 21:31:05 +1000

    feat: add Banner component to enhance the Home page layout

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from '../components/Hero'
 import FeaturedSection from '../components/FeaturedSection'
+import Banner from '../components/Banner'
 
 const Home = () => {
   return (
     <>
       <Hero />
       <FeaturedSection />
+      <Banner />
     </>
   )
 }
