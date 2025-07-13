556324120a25f9cd23e73e1a99dd978d6fb35099
waibaBikash <kavrelibikash@gmail.com>
2025-07-13 21:06:57 +1000

    style: enhance formatting and readability in Hero component

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -5,11 +5,16 @@ const Hero = () => {
 
   const [PickupLocation, setPickupLocation] = useState('');
   return (
-    <div className='h-screen flex flex-col items-center justify-center gap-14 bg-light text-center'>
+    <div 
+     className='h-screen flex flex-col items-center justify-center gap-14 bg-light text-center'>
         
-        <h1 className='text-4xl md:text-5xl font-semibold'>Luxury cars on Rent</h1>
+        <h1
+         className='text-4xl md:text-5xl font-semibold'>
+          Luxury cars on Rent
+          </h1>
 
-        <form className='flex flex-col md:flex-row items-start md:items-center justify-between p-6 rounded-lg md:rounded-full w-full max-w-80 md:max-w-200 bg-white shadow-[0px_8px_20px_rgba(0,0,0,0.1)]'> 
+        <form 
+         className='flex flex-col md:flex-row items-start md:items-center justify-between p-6 rounded-lg md:rounded-full w-full max-w-80 md:max-w-200 bg-white shadow-[0px_8px_20px_rgba(0,0,0,0.1)]'> 
            
             <div className='flex flex-col md:flex-row items-start md:items-center gap-10 min-md:ml-8'>
                 <div className='flex flex-col items-start gap-2'>
