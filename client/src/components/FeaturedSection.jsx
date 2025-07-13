d0275a955928c9023c962057454339c099ade917
waibaBikash <kavrelibikash@gmail.com>
2025-07-13 21:07:40 +1000

    style: improve formatting and readability in FeaturedSection component

--- client/src/components/FeaturedSection.jsx
+++ client/src/components/FeaturedSection.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
-import Title from './Title'
-import { assets, dummyCarData } from '../assets/assets'
-import CarCard from './CarCard'
-import { useNavigate } from 'react-router-dom'
+import Title from './Title';
+import { assets, dummyCarData } from '../assets/assets';
+import CarCard from './CarCard';
+import { useNavigate } from 'react-router-dom';
 
 const FeaturedSection = () => {
 
   const navigate = useNavigate();
   return (
-    <div className='flex flex-col items-center py-24 px-6 md:px-16 lg:px-24 xl:px-32'>
+    <div
+      className='flex flex-col items-center py-24 px-6 md:px-16 lg:px-24 xl:px-32'>
       <div>
-        <Title title='Featured Vehicles' subTitle='Explore our selection of premium vehicles available for your next advnture.' />
+        <Title title='Featured Vehicles'
+       subTitle='Explore our selection of premium vehicles available for your next advnture.' />
       </div>
 
        <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 mt-18'>
@@ -26,7 +28,8 @@ const FeaturedSection = () => {
           navigate('/cars'); scrollTo(0, 0);
         }}
          className='flex items-center justify-center gap-2 px-6 py-2 border border-borderColor hover:bg-gray-50 rounded-md mt-18 cursor-pointer'>
-          Explore all cars <img src={assets.arrow_icon} alt="arrow" />
+          Explore all cars
+           <img src={assets.arrow_icon} alt="arrow" />
         </button>
     </div>
   )
