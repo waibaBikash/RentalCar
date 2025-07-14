6279789183b917fd387be7de2a0ad0b3c29eaed8
waibaBikash <kavrelibikash@gmail.com>
2025-07-14 22:35:21 +1000

    style: improve formatting of Testimonial component for better readability

--- client/src/components/Testimonial.jsx
+++ client/src/components/Testimonial.jsx
@@ -26,11 +26,14 @@ const Testimonial = () => {
 
   return (
     <div className="py-28 px-6 md:px-16 lg:px-24 xl:px-44">
-      <Title title='What Our Customer Say' subTitle='Discover why discerning travelers choose StayVenture for their luxury accomodations around the world.'/>
+      <Title 
+      title='What Our Customer Say'
+       subTitle='Discover why discerning travelers choose StayVenture for their luxury accomodations around the world.'/>
 
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 mt-18">
                 {testimonials.map((testimonial, index) => (
-                    <div key={index} className="bg-white p-6 rounded-xl shadow-lg hover:translate-y-1 transition-all duration-500">
+                    <div key={index} 
+                    className="bg-white p-6 rounded-xl shadow-lg hover:translate-y-1 transition-all duration-500">
                         <div className="flex items-center gap-3">
                             <img className="w-12 h-12 rounded-full" src={testimonial.image} alt={testimonial.name} />
                             <div>
