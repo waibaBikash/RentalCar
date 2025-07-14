f5a3e368a8ac3fb438892d99359e9df11f1c7ad5
waibaBikash <kavrelibikash@gmail.com>
2025-07-14 22:35:54 +1000

    style: improve formatting of Newsletter component for better readability

--- client/src/components/Newsletter.jsx
+++ client/src/components/Newsletter.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 
 const Newsletter = () => {
   return (
-    <div className="flex flex-col items-center justify-center text-center space-y-2 max-md:px-4 my-10 mb-40">
+    <div 
+        className="flex flex-col items-center justify-center text-center space-y-2 max-md:px-4 my-10 mb-40">
             <h1 className="md:text-4xl text-2xl font-semibold">Never Miss a Deal!</h1>
             <p className="md:text-lg text-gray-500/70 pb-8">
                 Subscribe to get the latest offers, new arrivals, and exclusive discounts
@@ -14,7 +15,8 @@ const Newsletter = () => {
                     placeholder="Enter your email id"
                     required
                 />
-                <button type="submit" className="md:px-12 px-8 h-full text-white bg-primary hover:bg-primary-dull transition-all cursor-pointer rounded-md rounded-l-none">
+                <button type="submit" 
+                className="md:px-12 px-8 h-full text-white bg-primary hover:bg-primary-dull transition-all cursor-pointer rounded-md rounded-l-none">
                     Subscribe
                 </button>
             </form>
