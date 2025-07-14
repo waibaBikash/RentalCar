c199445a10a5c2f35b0fd4f7ef9720110c7de552
waibaBikash <kavrelibikash@gmail.com>
2025-07-14 20:25:17 +1000

    feat: add Newsletter component to allow users to subscribe for updates

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Hero from '../components/Hero'
 import FeaturedSection from '../components/FeaturedSection'
 import Banner from '../components/Banner'
 import Testimonial from '../components/Testimonial'
+import Newsletter from '../components/Newsletter'
 
 const Home = () => {
   return (
@@ -11,6 +12,7 @@ const Home = () => {
       <FeaturedSection />
       <Banner />
       <Testimonial />
+      <Newsletter />
     </>
   )
 }
