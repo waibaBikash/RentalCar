144949088ec6bfbaf63065ac7d3e3fbd962cd384
waibaBikash <kavrelibikash@gmail.com>
2025-07-14 22:34:50 +1000

    style: improve formatting of CarDetails component for better readability

--- client/src/pages/CarDetails.jsx
+++ client/src/pages/CarDetails.jsx
@@ -15,15 +15,18 @@ const CarDetails = () => {
   },[id]);
   return car ?  (
     <div className='px-6 md:px-16 lg:px-24 xl:px-32 mt-16'>
-      <button onClick={()=> navigate(-1)} className='flex items-center gap-2 mb-6 text-gray-500 cursor-pointer '>
-         <img src={assets.arrow_icon} alt="" className='rotate-180 opacity-65' />
+      <button onClick={()=> navigate(-1)} 
+      className='flex items-center gap-2 mb-6 text-gray-500 cursor-pointer '>
+         <img src={assets.arrow_icon} alt="" 
+         className='rotate-180 opacity-65' />
          Back to all cars
       </button>
        
        <div className='grid grid-cols-1 lg:grid-cols-3 gap-8 lg:gap-12'>
            {/* Left Car Image & Details */}
             <div className='lg:col-span-2'>
-               <img src={car.image} alt="" className='w-full h-auto md:mask-x-to-fuchsia-100 object-cover rounded-xl mb-6 shadow-md'/>
+               <img src={car.image} alt="" 
+               className='w-full h-auto md:mask-x-to-fuchsia-100 object-cover rounded-xl mb-6 shadow-md'/>
                  <div className='space-y-6'>
                      <div>
                        <h2 className='text-3xl font-bold'>{car.brand} {car.model}</h2>
