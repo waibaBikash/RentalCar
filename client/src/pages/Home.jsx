340457b3e392d1ee8653801959c8ccdd432a0f8e
waibaBikash <kavrelibikash@gmail.com>
2025-07-14 20:17:08 +1000

    feat: add Testimonial component to showcase customer feedback on the Home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Hero from '../components/Hero'
 import FeaturedSection from '../components/FeaturedSection'
 import Banner from '../components/Banner'
+import Testimonial from '../components/Testimonial'
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <Hero />
       <FeaturedSection />
       <Banner />
+      <Testimonial />
     </>
   )
 }
