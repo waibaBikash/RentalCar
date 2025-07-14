d8f058147468c4e257f7c16bb4ca9de2a34c5d66
waibaBikash <kavrelibikash@gmail.com>
2025-07-14 20:59:15 +1000

    style: improve footer links formatting for better readability

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -52,8 +52,10 @@ const Footer = () => {
             <div className='flex flex-col md:flex-row gap-2 items-center justify-between py-5'>
                 <p>© {new Date().getFullYear()} Brand. All rights reserved.</p>
                 <ul className='flex items-center gap-4'>
-                    <li><a href="#">Privacy <span> | </span></a></li>
-                    <li><a href="#">Terms <span> | </span></a></li>
+                    <li><a href="#">Privacy</a></li>
+                    <li> | </li>
+                    <li><a href="#">Terms</a></li>
+                    <li> | </li>
                     <li><a href="#">Cookies</a></li>
                 </ul>
             </div>
