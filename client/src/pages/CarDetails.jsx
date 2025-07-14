4ddf588e22ec20673cc4241941d3ab898c5d6d05
waibaBikash <kavrelibikash@gmail.com>
2025-07-14 22:24:33 +1000

    feat: implement CarDetails component to display car information and booking options

--- client/src/pages/CarDetails.jsx
+++ client/src/pages/CarDetails.jsx
@@ -1,11 +1,78 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
+import { assets, dummyCarData } from '../assets/assets';
 
 const CarDetails = () => {
-  return (
-    <div>
-      
+
+  const  { id } = useParams();
+  const navigate = useNavigate();
+  const [car , setCar] = useState(null);
+  
+
+  useEffect(() => {
+  setCar(dummyCarData.indexOf(car => car._id === id));
+  },[id]);
+  return car ?  (
+    <div className='px-6 md:px-16 lg:px-24 xl:px-32 mt-16'>
+      <button onClick={()=> navigate(-1)} className='flex items-center gap-2 mb-6 text-gray-500 cursor-pointer '>
+         <img src={assets.arrow_icon} alt="" className='rotate-180 opacity-65' />
+         Back to all cars
+      </button>
+       
+       <div className='grid grid-cols-1 lg:grid-cols-3 gap-8 lg:gap-12'>
+           {/* Left Car Image & Details */}
+            <div className='lg:col-span-2'>
+               <img src={car.image} alt="" className='w-full h-auto md:mask-x-to-fuchsia-100 object-cover rounded-xl mb-6 shadow-md'/>
+                 <div className='space-y-6'>
+                     <div>
+                       <h2 className='text-3xl font-bold'>{car.brand} {car.model}</h2>
+                       <p className='text-gray-500 text-lg'>{car.category} . {car.year}</p>
+                     </div>
+                     <hr className='border-borderColor my-6' />
+
+                      <div className='grid grid-cols-2 sm:grid-cols-4 gap-4'>
+                       {[
+                        {icon: assets.users_icon, text: `${car.seating_capacity} Seats`},
+                        {icon: assets.fuel_icon, text: car.fuel_type},
+                        {icon: assets.car_icon, text: car.transmission},
+                        {icon: assets.location_icon, text: car.location},
+                       ].map(({icon, text}) => (
+                        <div key={text} className='flex flex-col items-center bg-light p-4 rounded-lg'>
+                           <img src={icon} alt="" className='h-5 mb-2' />
+                           {text}
+                        </div>
+                       ))}
+                      </div>
+
+                      {/* Description */}
+                      <div>
+                         <h1 className='text-xl font-medium mb-3'>Description</h1>
+                         <p className='text-gray-500'>{car.description}</p>
+                      </div>
+                      {/* Features */}
+
+                      <div>
+                         <h1 className='text-xl font-medium mb-3'>Features</h1>
+                         <ul className='grid grid-cols-1 sm:grid-cols-2'>
+                           {
+                            ["360 Camera", "Bluetooth", "GPS", "Heated Seats", "Rear View Mirror"].map((item)=>(
+                              <li key={item} className='flex items-center text-gray-500'>
+                                <img src={assets.check_icon} alt="" className='h-4 mr-2' />
+                                {item}
+                              </li>
+                            ))
+                           }
+                         </ul>
+                      </div>
+                 </div>
+            </div>
+
+            {/* Right: Booking Form */}
+            <form></form>
+       </div>
+
     </div>
-  )
+  ) : <p>Loading...</p>
 }
 
 export default CarDetails
