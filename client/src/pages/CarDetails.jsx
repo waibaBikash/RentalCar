c61f2367c4da011619c3c29fe93d467e0e338e35
waibaBikash <kavrelibikash@gmail.com>
2025-07-15 23:26:36 +1000

    style: improve formatting and readability of booking form in CarDetails component

--- client/src/pages/CarDetails.jsx
+++ client/src/pages/CarDetails.jsx
@@ -75,22 +75,34 @@ const CarDetails = () => {
             </div>
 
             {/* Right: Booking Form */}
-            <form onSubmit={handleSubmit} className='shadow-lg h-max sticky top-18 rounded-xl p-6 space-y-6 text-gray-500'>
-                <p className='flex items-center justify-between text-2xl text-gray-800 font-semibold'>{currency}{car.pricePerDay} <span className='text-base text-gray-400 font-normal'>per day</span></p>
+            <form onSubmit={handleSubmit} 
+            className='shadow-lg h-max sticky top-18 rounded-xl p-6 space-y-6 text-gray-500'>
+                <p className='flex items-center justify-between text-2xl text-gray-800 font-semibold'>{currency}{car.pricePerDay}
+                   <span className='text-base text-gray-400 font-normal'>per day</span>
+                   </p>
 
                 <hr className='border-borderColor my-6' />
                 <div className='flex flex-col gap-2'>
                    <label htmlFor="pickup-date">Pickup Date</label>
-                   <input type="date" className='border border-borderColor px-3 py-2 rounded-lg' required id='pickup-date' min={new Date().toISOString().split('T')[0]} />
+                   <input type="date"
+                    className='border border-borderColor px-3 py-2 rounded-lg' 
+                    required id='pickup-date' min={new Date().toISOString().split('T')[0]} 
+                    />
                 </div>
                 <div className='flex flex-col gap-2'>
                    <label htmlFor="return-date">Return Date</label>
-                   <input type="date" className='border border-borderColor px-3 py-2 rounded-lg' required id='return-date' />
+                   <input type="date" 
+                   className='border border-borderColor px-3 py-2 rounded-lg'
+                    required id='return-date' />
                 </div>
 
-                <button className='w-full bg-primary hover:bg-primary-dull transition-all py-3 font-medium text-white rounded-xl cursor-pointer'>Book Now</button>
+                <button 
+                className='w-full bg-primary hover:bg-primary-dull transition-all py-3 font-medium text-white rounded-xl cursor-pointer'>Book Now</button>
 
-                <p  className='text-center text-sm'>No credit card required to reserve</p>
+                <p 
+                 className='text-center text-sm'>
+                  No credit card required to reserve
+                 </p>
             </form>
        </div>
 
@@ -98,4 +110,4 @@ const CarDetails = () => {
   ) : <Loading />
 }
 
-export default CarDetails
+export default CarDetails;
