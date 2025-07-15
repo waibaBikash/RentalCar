d467959662146f385d58d60e55bd4f5cd04d684b
waibaBikash <kavrelibikash@gmail.com>
2025-07-15 23:24:52 +1000

    style: improve formatting and readability of Cars component

--- client/src/pages/Cars.jsx
+++ client/src/pages/Cars.jsx
@@ -10,21 +10,31 @@ const Cars = () => {
     <div>
 
       <div className='flex flex-col items-center py-20 bg-light max-md:px-4'>
-        <Title title='Available Cars' subTitle='Browse our selection of premium vehicles available for your next advnenture' />
+        <Title 
+        title='Available Cars' 
+        subTitle='Browse our selection of premium vehicles available for your next advnenture' />
 
         <div className='flex items-center bg-white px-4 mt-6 max-w-140 w-full h-12 rounded-full shadow'>
-          <img src={assets.search_icon} alt="" className='w-4.5 h4.5 mr-2' />
-          <input onClick={(e)=> setInput(e.target.value)} value={input} type="text" placeholder='Search by make, mopdel, or features' className='w-full h-full outline-none text-gray-500' />
-          <img src={assets.filter_icon} alt="" className='w-4.5 h4.5 ml-2' />
+          <img src={assets.search_icon} alt="" 
+          className='w-4.5 h4.5 mr-2' />
+          <input onClick={(e)=> setInput(e.target.value)} 
+          value={input}
+           type="text" placeholder='Search by make, mopdel, or features' 
+           className='w-full h-full outline-none text-gray-500' />
+          <img src={assets.filter_icon} alt="" 
+          className='w-4.5 h4.5 ml-2' />
            
         </div>
 
       </div>
 
       <div className='px-6 md:px-16 lg:px-24 xl:px-32 mt-10'>
-        <p>showing {dummyCarData.length} Cars</p>
+        <p className='text-gray-500 xl:px-20 max-w-7xl mx-auto'>
+          showing {dummyCarData.length} Cars
+          </p>
          
-         <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 mt-4 xl:px-20 max-w-7xl mx-auto'>
+         <div 
+         className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 mt-4 xl:px-20 max-w-7xl mx-auto'>
             {dummyCarData.map((car, index)=> (
               <div key={index}>
                 <CarCard car={car} />
@@ -37,4 +47,4 @@ const Cars = () => {
   )
 }
 
-export default Cars
+export default Cars;
