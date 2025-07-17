cd47c91dc91e8f5203db82ed7ddf4f3c1b34e9e4
waibaBikash <kavrelibikash@gmail.com>
2025-07-17 23:06:19 +1000

    style: improve formatting and readability in Dashboard component

--- client/src/pages/owner/Dashboard.jsx
+++ client/src/pages/owner/Dashboard.jsx
@@ -1,4 +1,4 @@
-import React, { use, useEffect, useState } from 'react'
+import React, { use, useEffect, useState } from 'react';
 import { assets, dummyDashboardData } from '../../assets/assets';
 import Title from '../../components/owner/Title';
 
@@ -31,17 +31,24 @@ const Dashboard = () => {
 
   return (
     <div className='px-4 pt-10 md:px-10 flex-1'>
-      <Title title='Admin Dashboard' subTitle='Monitor overall platform performance including total cars, bookings, revenue, and recent activities' />
+      <Title title='Admin Dashboard'
+       subTitle='Monitor overall platform performance including total cars, bookings, revenue, and recent activities' />
 
        <div  className='grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 my-8 max-w-3xl'>
          {dashboardCards.map((card, index) => (
            <div key={index} className='flex gap-2 items-center justify-between p-4 rounded-md border border-borderColor'>
                <div>
-                  <h1  className='text-xs text-gray-500'>{card.title}</h1>
-                  <p  className='text-lg font-semibold'>{card.value}</p>
+                  <h1 
+                   className='text-xs text-gray-500'>{card.title}
+                   </h1>
+                  <p 
+                   className='text-lg font-semibold'>{card.value}
+                   </p>
                </div>
-                <div className='flex items-center justify-center w-10 h-10 rounded-full bg-primary/10'>
-                   <img src={card.icon} alt="" className='w-4 h-4' />
+                <div 
+                 className='flex items-center justify-center w-10 h-10 rounded-full bg-primary/10'>
+                   <img src={card.icon} alt=""
+                    className='w-4 h-4' />
                 </div>
            </div>
          ))}
@@ -56,7 +63,8 @@ const Dashboard = () => {
                   <div key={index} className='mt-4 flex items-center justify-between'>
                     <div className='flex items-center gap-2'>
                       <div className='hidden md:flex items-center justify-center w-12 h-12 rounded-full bg-primary/10'>
-                         <img src={assets.listIconColored} alt=""  className='h-5 w-5'/>
+                         <img src={assets.listIconColored} alt=""
+                         className='h-5 w-5'/>
                       </div>
                        <div>
                          <p>{booking.car.brand} {booking.car.model}</p>
@@ -81,4 +89,4 @@ const Dashboard = () => {
   )
 }
 
-export default Dashboard
+export default Dashboard;
