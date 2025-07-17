1c2b7711b5d7afbe892a1f599a1d55d6261cf9f4
waibaBikash <kavrelibikash@gmail.com>
2025-07-17 23:06:40 +1000

    style: fix formatting and add missing semicolons in Layout component

--- client/src/pages/owner/Layout.jsx
+++ client/src/pages/owner/Layout.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import NavbarOwner from '../../components/owner/NavbarOwner'
-import Sidebar from '../../components/owner/Sidebar'
-import { Outlet } from 'react-router-dom'
+import NavbarOwner from '../../components/owner/NavbarOwner';
+import Sidebar from '../../components/owner/Sidebar';
+import { Outlet } from 'react-router-dom';
 
 const Layout = () => {
   return (
@@ -16,4 +16,4 @@ const Layout = () => {
   )
 }
 
-export default Layout
+export default Layout;
