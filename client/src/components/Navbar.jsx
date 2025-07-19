6171fb5a0bb6f1f5f9dae52fabdde1febb57e923
waibaBikash <kavrelibikash@gmail.com>
2025-07-19 23:08:14 +1000

    style: improve formatting and readability in Navbar component

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -14,7 +14,8 @@ const Navbar = ({setShowLogin}) => {
        src={assets.logo} alt="logo" className='h-8' />
       </Link>
 
-      <div className={`max-sm:fixed max-sm:h-screen max-sm:w-full max-sm:top-16 max-sm:border-t border-borderColor right-0 flex flex-col sm:flex-row items-start sm:items-center gap-4 sm:gap-8 max-sm:p-4 transition-all duration-300 z-50 ${location.pathname === '/' ? 'bg-light' : 'bg-white'} ${open ? 'max-sm:translate-x-0' : 'max-sm:translate-x-full'}`}>
+      <div
+       className={`max-sm:fixed max-sm:h-screen max-sm:w-full max-sm:top-16 max-sm:border-t border-borderColor right-0 flex flex-col sm:flex-row items-start sm:items-center gap-4 sm:gap-8 max-sm:p-4 transition-all duration-300 z-50 ${location.pathname === '/' ? 'bg-light' : 'bg-white'} ${open ? 'max-sm:translate-x-0' : 'max-sm:translate-x-full'}`}>
          {menuLinks.map((link, index)=>(
           <Link key={index} to={link.path}>
             {link.name}
@@ -31,8 +32,11 @@ const Navbar = ({setShowLogin}) => {
          </div>
 
            <div className='flex max-sm:flex-col items-start sm:items-center gap-6'>
-             <button onClick={()=> navigate('/owner')} className='cursor-pointer'>Dashboard</button>
-             <button onClick={()=> setShowLogin(true) } className='cursor-pointer px-8 py-2 bg-primary hover:bg-primary-dull transition-all text-white rounded-lg'>Login</button>
+             <button onClick={()=> navigate('/owner')}
+              className='cursor-pointer'>Dashboard</button>
+             <button onClick={()=> setShowLogin(true) } 
+             className='cursor-pointer px-8 py-2 bg-primary hover:bg-primary-dull transition-all text-white rounded-lg'>
+              Login</button>
            </div>
       </div>
         
