5344338ae65f004645d90d185cc6968b7ad632c5
waibaBikash <kavrelibikash@gmail.com>
2025-07-27 20:01:06 +1000

    create api endpoint

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import 'dotenv/config'
 import cors from 'cors'
 import connetDB from './configs/db.js';
+import userRouter from './routes/userRoutes.js';
 
 // Initialize Express App
 
@@ -15,6 +16,8 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res)=> res.send("Server is running"))
+
+app.use('/api/user', userRouter)
 
 const PORT = process.env.PORT || 3000;
 
