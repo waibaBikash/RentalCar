23744e710932f4fa5b365f98f14d796220df0445
waibaBikash <kavrelibikash@gmail.com>
2025-07-27 19:55:45 +1000

    controller function for user login

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -30,3 +30,24 @@ export const registerUser = async (req, res)=> {
     res.json({success: false, message: error.message})
   }
 }
+
+// Login User
+
+export const loginUser = async(req, res)=>{
+  try {
+     const {email, password} = req.body
+     const user = await User.findOne({email})
+     if (!user) {
+      return res.json({success: false, message: 'User not found'})
+     }
+     const isMatch = await bcrypt.compare(password, user.password)
+     if(!isMatch){
+       return res.json({success: false, message: "Invalid Credentials"})
+     }
+     const token = generateToken(user._id.toString())
+     res.json({success: true, token})
+  } catch (error) {
+    console.log(error.message)
+    res.json({success: false, message: error.message})
+  }
+}
