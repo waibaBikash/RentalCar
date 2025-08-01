076b2a0c4a9da3926f7157cfa6a3f1a56396aeea
waibaBikash <kavrelibikash@gmail.com>
2025-08-01 21:12:13 +1000

    route to add car

--- server/routes/ownerRoutes.js
+++ server/routes/ownerRoutes.js
@@ -1,11 +1,13 @@
 import express from 'express'
 import { protect } from '../middleware/auth.js';
-import { changeRoleToOwner } from '../controllers/ownerController.js';
+import { addCar, changeRoleToOwner } from '../controllers/ownerController.js';
+import upload from "../middleware/multer.js"
 
 
 const ownerRouter = express.Router();
 
 ownerRouter.post("/change-role", protect, changeRoleToOwner)
+ownerRouter.post("/add-car", upload.single("image") , protect, addCar, changeRoleToOwner)
 
 
 export default ownerRouter;
