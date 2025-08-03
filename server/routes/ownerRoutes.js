253b4d61aab30836f790f400fe801728e3fdc2fb
waibaBikash <kavrelibikash@gmail.com>
2025-08-03 20:47:57 +1000

    Create a Api route for owner

--- server/routes/ownerRoutes.js
+++ server/routes/ownerRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import { protect } from '../middleware/auth.js';
-import { addCar, changeRoleToOwner } from '../controllers/ownerController.js';
+import { addCar, changeRoleToOwner, deleteCar, getOwnerCars, toggleCarAvailability } from '../controllers/ownerController.js';
 import upload from "../middleware/multer.js"
 
 
@@ -8,6 +8,10 @@ const ownerRouter = express.Router();
 
 ownerRouter.post("/change-role", protect, changeRoleToOwner)
 ownerRouter.post("/add-car", upload.single("image") , protect, addCar, changeRoleToOwner)
+ownerRouter.get("/cars" , protect, getOwnerCars)
+ownerRouter.post("/toggle-car" , protect, toggleCarAvailability)
+ownerRouter.post("/toggle-car" , protect, toggleCarAvailability)
+ownerRouter.post("/delete-car" , protect, deleteCar)
 
 
 export default ownerRouter;
