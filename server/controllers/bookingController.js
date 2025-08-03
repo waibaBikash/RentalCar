ee828a6d9334c1c9dcc426b55c2f1e7d525d14a7
waibaBikash <kavrelibikash@gmail.com>
2025-08-03 21:14:36 +1000

    Function to check Availability of car for a given Data

--- server/controllers/bookingController.js
+++ server/controllers/bookingController.js
@@ -1,5 +1,7 @@
 
+import { Promise } from "mongoose";
 import Booking from "../models/Booking.js"
+import Car from "../models/Car.js";
 
 // Function to check Availability of car for a given Data
 
@@ -13,3 +15,28 @@ const checkAvailability = async (car, pickupDate, returnDate)=>{
   return bookings.length === 0;
 }
 
+// API to Check Availabilituy of Cars for the given Date and Location
+
+export const checkAvailabilityOfCar = async ()=>{
+   try {
+     const {location, pickupData, returnData}= req.body;
+
+     // fetch all availability cars for the given location
+     const cars = await Car.find({location, isAvailable: true})
+
+     // check car availability for the given date range using promise
+     const checkAvailableCarPromises = cars.map(async(car)=>{
+       const isAvailable = await checkAvailability(car._id, pickupData, returnData)
+       return {...car._doc, isAvailable: isAvailable}
+     })
+
+     let availableCars = await Promise.all(checkAvailableCarPromises);
+     availableCars = availableCars.filter(car => car.isAvailable === true)
+
+     res.json({success: true, availableCars})
+
+   } catch (error) {
+    console.log(error.message);
+    res.json({success: false, message: error.message})
+   }
+}
