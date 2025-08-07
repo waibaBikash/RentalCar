82f1ceeaa451156b71626981430369864405288a
waibaBikash <kavrelibikash@gmail.com>
2025-08-07 20:35:53 +1000

    complete all the bacend api

--- server/routes/ownerRoutes.js
+++ server/routes/ownerRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import { protect } from '../middleware/auth.js';
-import { addCar, changeRoleToOwner, deleteCar, getDashboardData, getOwnerCars, toggleCarAvailability } from '../controllers/ownerController.js';
+import { addCar, changeRoleToOwner, deleteCar, getDashboardData, getOwnerCars, toggleCarAvailability, updateUserImage } from '../controllers/ownerController.js';
 import upload from "../middleware/multer.js"
 
 
@@ -14,6 +14,7 @@ ownerRouter.post("/toggle-car" , protect, toggleCarAvailability)
 ownerRouter.post("/delete-car" , protect, deleteCar)
 
 ownerRouter.get('/dashboard', protect, getDashboardData)
+ownerRouter.post('/update-image', upload.single("image"), protect, updateUserImage)
 
 
 export default ownerRouter;
